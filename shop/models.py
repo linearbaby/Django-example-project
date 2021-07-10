1a9d7b4f44b9b969396ecfe68ec4359f6e69471b
Artem Gotovtsev <artemiygotovtsev@gmail.com>
2021-07-10 04:43:24 +0300

    Fixed admin interface, some template mapping.

--- shop/models.py
+++ shop/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'categories'
 
+    def __str__(self):
+        return self.name
+
 
 class Products(models.Model):
     category = models.ForeignKey(Category, models.CASCADE, 'product')
@@ -22,3 +25,6 @@ class Products(models.Model):
 
     def get_absolute_url(self):
         return reverse('shop:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.name
